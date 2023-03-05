63633cce26168cf5ce6108dbcc5721f03c04cbc2
ZaidArman <zahidu864@gmail.com>
2023-03-05 13:10:27 +0500

    New Changes in Python Practice

---
+++ Excercise/table.py
@@ -0,0 +1,10 @@
+"""
+Excercise-1:
+Write a program which does the following:
+- Take a number input from the console sample input
+- We need to output to the console the multiplication table for that number using the "for" loop in the format given in the IDE
+"""
+
+a = int(input(" Enter a number for table: "))
+for i in range(1,11):
+  print(" Table of ", a, 'x', i, '=', a * i)
